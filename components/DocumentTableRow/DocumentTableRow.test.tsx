5b3a8cd52d841bcfc81e2a74a41a26b71acc2c91
iAlbertTran <albert@formswift.com>
2022-09-08 16:06:31 -0700

    test: add scaffolding for writing the rtl version of the enzyme test
    Refs kingstreetlabs/KingStreetLabs#10985

--- components/DocumentTableRow/DocumentTableRow.test.tsx
+++ components/DocumentTableRow/DocumentTableRow.test.tsx
@@ -1,6 +1,9 @@
 import Enzyme, { mount } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import sinon from 'sinon';
+
+import { screen, render } from '@testing-library/react';
+
 import { DocumentTableRow } from './DocumentTableRow';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -11,9 +14,15 @@ describe('<DocumentTableRow />', () => {
     removeDocFromList: sinon.stub(),
   }
 
-  it('renders correctly', () => {
+  // Enzyme version
+  it('renders the name of the document', () => {
     const documentTableRow = mount(<DocumentTableRow {...props} />);
     const name = documentTableRow.find('td').at(0);
     expect(name.text()).toEqual(props.name);
+  });
+
+  // RTL version
+  it('renders the name of the document', () => {
+
   });
 });
